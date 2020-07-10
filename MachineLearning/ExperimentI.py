366f84e18fffa9ce96dbcf2b30934e2129fcd345
zibrahim <zina.ibrahim@gmail.com>
2020-07-10 10:18:29 +0100

    Fix 30-day mortality not showing in 4-Aggregate And trained XGBoost on vitals only. Fixed ROC curve generation. Need to a) think of the featues, b) feature importance and shap plots.

--- MachineLearning/ExperimentI.py
+++ MachineLearning/ExperimentI.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import metrics
+from sklearn.metrics import roc_curve, auc
 
 #from sklearn.model_selection import GroupKFold
 
@@ -20,6 +20,11 @@ class ExperimentI:
         distrs = [get_distribution(y)]
         index = ['Entire set']
 
+        tprs = []
+        aucs = []
+        mean_fpr = np.linspace(0, 1, 100)
+        plt.figure(figsize=(10, 10))
+
         for fold_ind, (training_ind, testing_ind) in enumerate(stratified_group_k_fold(X, y, groups, k=100)) :
             training_groups, testing_groups = groups[training_ind], groups[testing_ind]
             training_y, testing_y= y[training_ind], y[testing_ind]
@@ -29,17 +34,17 @@ class ExperimentI:
            # print(type(training_groups), type(testing_groups))
             assert len(set(training_groups) & set(testing_groups)) == 0
 
-
             #Train, predict and Plot
-            fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-            xgbm = xgbm.fit(training_X, training_y)
-            y_pred = xgbm.predict_proba(testing_X)[:, 1]
-            fpr, tpr, threshold = metrics.roc_curve(testing_y, y_pred)
-            roc_auc = metrics.auc(fpr, tpr)
-            plt.plot(fpr, tpr, 'r', label='XGBoost (AUC:%0.3F)' % roc_auc, linestyle='-')
-            plt.legend(loc='lower right', frameon=False)
-            plt.savefig("plots.pdf")
+            xgbm.fit(training_X, training_y)
+            y_pred_rt = xgbm.predict_proba(testing_X)[:, 1]
 
+            fpr, tpr, thresholds = roc_curve(testing_y, y_pred_rt)
+            tprs.append(np.interp(mean_fpr, fpr, tpr))
+            tprs[-1][0] = 0.0
+            roc_auc = auc(fpr, tpr)
+            aucs.append(roc_auc)
+            plt.plot(fpr, tpr, lw=1, alpha=0.3,
+                     label='ROC fold %d (AUC = %0.2f)' % (fold_ind, roc_auc))
 
             #add to the distribution dataframe, for verification purposes
             distrs.append(get_distribution(training_y))
@@ -48,6 +53,31 @@ class ExperimentI:
             distrs.append(get_distribution(testing_y))
             index.append(f'testing set - fold {fold_ind}')
 
+        #Finallise ROC curve
+        plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+                 label='Chance', alpha=.8)
+
+        mean_tpr = np.mean(tprs, axis=0)
+        mean_tpr[-1] = 1.0
+        mean_auc = auc(mean_fpr, mean_tpr)
+        std_auc = np.std(aucs)
+        plt.plot(mean_fpr, mean_tpr, color='b',
+                 label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+                 lw=2, alpha=.8)
+
+        std_tpr = np.std(tprs, axis=0)
+        tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+        tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+        plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                         label=r'$\pm$ 1 std. dev.')
+
+        plt.xlim([-0.01, 1.01])
+        plt.ylim([-0.01, 1.01])
+        plt.xlabel('False Positive Rate', fontsize=18)
+        plt.ylabel('True Positive Rate', fontsize=18)
+        plt.title('Cross-Validation ROC of SVM', fontsize=18)
+        #plt.legend(loc="lower right", prop={'size' : 15})
+        plt.savefig("plots.pdf")
 
         distr_df = pd.DataFrame(distrs, index=index, columns=[f'Label {l}' for l in range(np.max(y) + 1)])
         distr_df.to_csv(stats_path+"Experiment1-K-Fold-Distributions.csv", index=True)
